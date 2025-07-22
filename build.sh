bedc9bb5ae9a4964c8f71b7ef08fcf561f648332
Erzbir <erzbir@mail.com>
2025-07-22 20:18:36 +0800

    add obfuscator

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
- npx esbuild src/index.js --bundle --minify --outfile=build/bundle.js
+npx esbuild src/index.js --bundle --minify --outfile=build/bundle.js
+npx javascript-obfuscator build/bundle.js --output dist/main.min.js \
+   --compact true \
+   --control-flow-flattening false \
+   --dead-code-injection false \
+   --string-array true \
+   --string-array-encoding base64 \
+   --string-array-threshold 1 \
+   --rename-globals true \
+   --self-defending false \
+   --disable-console-output true \
+   --transform-object-keys true
